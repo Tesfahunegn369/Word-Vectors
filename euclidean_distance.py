422f07b35b0209af29f9067e9bff592a4a2d25aa
Tesfahunegn Minwuyelet Mengistu <47448502+Tesfahunegn369@users.noreply.github.com>
2026-04-13 16:06:14 +0900

    Update euclidean_distance.py

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -1,6 +1,4 @@
 import numpy as np
-
-# Q2: Implement Euclidean distance function (10 pts)
 def euclidean(A, B):
     """
     Input:
@@ -9,19 +7,14 @@ def euclidean(A, B):
     Output:
         d: numerical number representing the Euclidean distance between A and B.
     """
-
-    ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
-
     # euclidean distance
     d = np.linalg.norm(A - B)
-
-    ### END CODE HERE ###
 
     return d
 
 def euclidean_distance_test(word_embeddings):
     # Testing your function
-    print("Q2: Implement Euclidean distance function (10 pts)")
+    print("Implement Euclidean distance function")
     if abs(euclidean(np.array([0, 0]), np.array([1, 1])) - np.sqrt(2.0)) < 0.001:
         print('SUCCESS')
     else:
